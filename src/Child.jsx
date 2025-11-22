1406d9fc6bd367fc9cca55a791abc2c7f6947604
Nareshmalviya09 <Nareshmalviya4321@gmail.com>
2025-11-22 16:32:13 +0530

    new file added

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Child = () => {
+const Child = ({data}) => {
+
+    let{name,age,batch}=data
   return (
     <>
     
         Child
+        <h3>my name is {name}</h3>
+        <h3>my age { age}</h3>
+        <h3>my Batch {batch}</h3>
     </>
   )
 }
--- src/props.jsx
+++ src/props.jsx
@@ -1,13 +1,24 @@
 import Child from "./Child"
 
 let Props =()=>{
+    //  let name ="naresh"
+    //  let age =25
+    //  let batch="p60"
 
+    let student={
+        name:"naresh",
+        age:25,
+        batch:"p60"
+    }
     return(
 
-        <div>
+        < >
             <h1> This is props</h1>
-            <Child/>
-        </div>
+            {/* <Child myname={name} myage={age} mybatch={batch}/>
+            <Child myname="praveen" myage={42} mybatch={batch}/> */}
+
+            <Child data={student}/>
+        </ >
     )
 }
 export default Props
